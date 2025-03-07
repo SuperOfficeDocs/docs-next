b259b0b6d307e23f9e4fbb81cdca6800c493ff8d
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-03-07 13:40:30 +0530

    feat: defined sample docs collection in config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,8 +16,21 @@ const releaseNotes = defineCollection({
   }).passthrough(),
 });
 
+const docs = defineCollection({
+  type: 'content',
+  schema: z.object({
+    // title: z.string(),
+    // date: z.date().optional(),
+    // description: z.string().optional(),
+    // author: z.string().optional(),
+    // version: z.string().optional(),
+    // version_mobile: z.string().optional(),
+  }).passthrough(),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'release-notes': releaseNotes,
+  'docs': docs,
 };
