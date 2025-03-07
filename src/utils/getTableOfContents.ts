df860697db9b31fa024e7531a2be1dce73f61b8e
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-03-07 13:52:44 +0530

    fix: updated getTableOfContent util

--- src/utils/getTableOfContents.ts
+++ src/utils/getTableOfContents.ts
@@ -4,8 +4,8 @@ import YAML from "yaml";
 import type { TocData } from "../types/TableOfContentTypes";
 
 // Read and parse YAML Data
-export function getTableOfContents(release: string): TocData {
-  const filePath = path.join("src/data/", `${release}.yml`);
+export function getTableOfContents(pathName: string): TocData {
+  const filePath = path.join("src/content/", `${pathName}.yml`);
   if (!fs.existsSync(filePath)) {
     throw new Error(`ToC file not found: ${filePath}`);
   }
