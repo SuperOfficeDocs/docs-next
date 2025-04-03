ff07dfc93cca1cb15ef84120e9796b12d0dcf876
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-04-03 09:41:40 +0530

    refactor: refactored getCategory and getSubCategory page utils

--- src/utils/getCategoryPageContents.ts
+++ src/utils/getCategoryPageContents.ts
@@ -2,23 +2,43 @@ import fs from "fs";
 import path from "path";
 import YAML from "yaml";
 import type { CategoryContentItem } from "../types/CategoryPageTypes";
+import type { SubCategoryContentItem } from "../types/SubCategoryPageTypes";
 
-function loadYamlFile(filePath: string): CategoryContentItem | null {
+function loadYamlFile(
+  pathName: string,
+  type: string
+): CategoryContentItem | SubCategoryContentItem | null {
+  const filePath = path.join("src/content/docs/", `${pathName}/index.yml`);
+
   if (!fs.existsSync(filePath)) {
     console.warn(`Warning: Category content file not found: ${filePath}`);
     return null;
   }
+
   const file = fs.readFileSync(filePath, "utf8");
-  return YAML.parse(file) as CategoryContentItem;
+
+  if (type == "category") return YAML.parse(file) as CategoryContentItem;
+  else return YAML.parse(file) as SubCategoryContentItem;
 }
 
 export function getCategoryPageContents(pathName: string): CategoryContentItem {
-  const filePath = path.join("src/content/docs/", `${pathName}/index.yml`);
-  const YAMLData = loadYamlFile(filePath);
+  const YAMLData = loadYamlFile(pathName, "category");
 
   if (!YAMLData) {
-    throw new Error(`Failed to load Category index file: ${filePath}`);
+    throw new Error(`Failed to load Category index file: ${pathName}`);
   }
 
-  return YAMLData;
+  return YAMLData as CategoryContentItem;
+}
+
+export function getSubCategoryPageContents(
+  pathName: string
+): SubCategoryContentItem {
+  const YAMLData = loadYamlFile(pathName, "subcategory");
+
+  if (!YAMLData) {
+    throw new Error(`Failed to load Category index file: ${pathName}`);
+  }
+
+  return YAMLData as SubCategoryContentItem;
 }
