1c38a6d65fc2629aaccaedf292c243c673317e47
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-04-21 11:42:57 +0530

    feat: webApi components

---
+++ src/types/WebApiLayoutTypes.ts
@@ -0,0 +1,8 @@
+export interface indexDictionaryProps {
+    Class: number[];
+    Constructor: number[];
+    Method : number[];
+    Property: number[];
+    Field: number[];
+    Namespace: number[];
+}
