1ccf4962dc833bf041d5ae6d06dffa1c16c9d3c9
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-04-24 14:23:20 +0530

    test: index page tests

---
+++ tests/index.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { heroItems } from '~/navigation';
+
+test('Check index page title', async ({ page }) => {
+    await page.goto("");
+    await expect(page).toHaveTitle('SuperOffice Docs');
+});
+
+
+test('Click on hero items', async({page}) => {
+    await page.goto("");
+
+    for (const link of heroItems.links) {
+        await page.getByRole('heading', { name: link.title , level: 2 }).click();
+        await expect(page).toHaveURL(`/docs-next${link.url}`);
+        await page.goBack()
+    }
+})
+
+
+test('Index page snapshot test', async ({ page }) => {
+    await page.goto("");
+    await expect(page).toHaveScreenshot();
+});
