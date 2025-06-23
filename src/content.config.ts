f078e180b9302a5748431f7153f233f95c5e3a94
Bergfrid Dias <bergfrid@digitialdias.com>
2025-06-23 17:22:16 +0200

    #152 Initial setup for contribution repo

--- src/content.config.ts
+++ src/content.config.ts
@@ -36,7 +36,27 @@ const DocsSchema = z.object({
 .passthrough().partial()
 // .partial() is used to make every property optional due to current frontmatter mismatch in some markdown files. Needs to be removed once frontmatter fixed
 
+const SimplifiedYamlSchema = z.object({
+  YamlMime: z.enum(["Category", "SubCategory"]), //- can't use as-is since it's a comment in our code
+  title: z.string(),
+  metadata: z.any(),
+}).passthrough(); // Allow all other fields like landingContent, conceptualContent, tools, etc.
 
+const TocItemSchema: z.ZodType<any> = z.lazy(() =>
+  z.object({
+    name: z.string(),
+    uid: z.string().optional(),
+    href: z.string().optional(),
+    topicHref: z.string().optional(),
+    items: z.array(TocItemSchema).optional(),
+  })
+);
+
+const TocYamlSchema = z.object({
+  items: z.array(TocItemSchema),
+});
+
+// Collections in src/content
 const releaseNotes = defineCollection({
   loader: glob({ pattern: ["*.md", "**/!(*includes*)/*.md"], base: "./src/content/release-notes" }),
   schema: DocsSchema,
@@ -57,6 +77,46 @@ const WebAPI = defineCollection({
   loader: glob({ pattern:["**/!(*toc).yml",], base:"./src/content/docs/en/api/reference/webapi"}),
 });
 
+const tocFilesInternal = defineCollection({
+  loader: glob({
+    pattern: ["**/toc.yml"],
+    base: "./src/content",
+  }),
+  schema: TocYamlSchema,
+});
+
+// Collections in external-content (cloned from another GitHub repo)
+
+const tocFilesExternal = defineCollection({
+  loader: glob({
+    pattern: ["contribution/**/toc.yml"], // Add in ext superoffice-docs later
+    base: "./external-content",
+  }),
+  schema: TocYamlSchema,
+});
+
+const externalLandingPages = defineCollection({
+  loader: glob({
+    pattern: [
+      "contribution/**/*.yml",
+      "!**/toc.yml",
+      // Add in ext superoffice-docs later
+    ],
+    base: "./external-content",
+  }),
+  schema: SimplifiedYamlSchema,
+});
+
+const contributionRepo = defineCollection({
+  loader: glob({
+    pattern: [
+      "**/*.md",
+      "!**/includes/**",
+      "!CODE_OF_CONDUCT.md"],
+    base: "./external-content/contribution",
+  }),
+  schema: DocsSchema,
+});
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
@@ -65,6 +125,8 @@ export const collections = {
   en: enDocs,
   de: deDocs,
   webapi: WebAPI,
+  contribute: contributionRepo,
+  external: externalLandingPages,
+  tocInternal: tocFilesInternal,
+  tocExternal: tocFilesExternal,
 };
-
-
