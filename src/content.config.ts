52e0e8afb1c90963d53aae11a492a86e5c6f270e
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-07-02 15:59:12 +0530

    feat: update github actions to clone so-docs repo

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,37 +3,32 @@ import { z, defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 
 const DocsSchema = z.object({
-  
+
   //Mandatory Properties
-  author : z.union([
-    z.string(), 
-    z.object({ "github-id" : z.string().nullable()}),
-  ]).nullable(),
-  date : z.coerce.string().nullable(),
-  title : z.string().nullable(),
-  uid : z.string().nullable(),
-  topic : z.string().nullable(),
-  
+  author: z.string().nullable(),
+  date: z.coerce.string().nullable(),
+  uid: z.string().nullable(),
 
   //Optional Properties
-  description : z.string().optional().nullable(),
-  audience : z.string().optional().nullable(),
-  audience_tooltip : z.string().optional().nullable(),
-  category : z.string().optional().nullable(),
-  updated : z.date().optional().nullable(),
-  version : z.coerce.string().optional().nullable(),
-  version_sofo : z.coerce.string().optional().nullable(),
-  version_devportal : z.coerce.string().optional().nullable(),
-  version_mobile : z.coerce.string().optional().nullable(),
-  translation_type : z.number().optional().nullable(),
-  envir : z.union([z.string(), z.array(z.string())]).optional().nullable(),
-  generated : z.boolean().optional().nullable(),
-  keywords : z.union([z.string(), z.array(z.string())]).optional().nullable(),
-  language : z.string().max(2).optional().nullable(),
-  pilot : z.string().optional().nullable(),
-  redirect_url : z.string().optional().nullable(),
+  title: z.string().optional().nullable(),
+  description: z.string().optional().nullable(),
+  audience: z.string().optional().nullable(),
+  audience_tooltip: z.string().optional().nullable(),
+  category: z.string().optional().nullable(),
+  updated: z.coerce.string().optional().nullable(),
+  version: z.coerce.string().optional().nullable(),
+  version_sofo: z.coerce.string().optional().nullable(),
+  version_devportal: z.coerce.string().optional().nullable(),
+  version_mobile: z.coerce.string().optional().nullable(),
+  translation_type: z.union([z.number(), z.string()]).optional().nullable(),
+  deployment: z.union([z.string(), z.array(z.string())]).optional().nullable(),
+  generated: z.coerce.string().optional().nullable(),
+  keywords: z.union([z.string(), z.array(z.string())]).optional().nullable(),
+  language: z.string().max(2).optional().nullable(),
+  pilot: z.string().optional().nullable(),
+  redirect_url: z.string().optional().nullable(),
 })
-.passthrough().partial()
+  .passthrough().partial()
 // .partial() is used to make every property optional due to current frontmatter mismatch in some markdown files. Needs to be removed once frontmatter fixed
 
 const SimplifiedYamlSchema = z.object({
@@ -56,40 +51,59 @@ const TocYamlSchema = z.object({
   items: z.array(TocItemSchema),
 });
 
+
 // Collections in src/content
 const releaseNotes = defineCollection({
-  loader: glob({ pattern: ["*.md", "**/!(*includes*)/*.md"], base: "./src/content/release-notes" }),
+  loader: glob({ 
+    // pattern: ["**/!(*includes*)/*.md"], 
+    pattern: [""], //Temporary disabled
+    base: "external-content/superoffice-docs/release-notes" }),
   schema: DocsSchema,
 });
 
 const enDocs = defineCollection({
-  loader: glob({ pattern: "**/!(**includes**)/*.md",  base: "./src/content/docs/en" }),
+  loader: glob({
+    pattern: [
+      "**/*.md",
+      "!api/**/*.md",
+      "!**/includes/**/*.md" 
+    ],
+    base: "external-content/superoffice-docs/docs/en",
+  }),
   schema: DocsSchema,
 });
 
 const deDocs = defineCollection({
-  loader: glob({ pattern: "**/!(**includes**)/*.md", base: "./src/content/docs/de" }),
+  loader: glob({ 
+    // pattern: "**/!(**includes**)/*.md", 
+    pattern: "", //Temporary disabled
+    base: "external-content/superoffice-docs/docs/de" }),
   schema: DocsSchema,
 });
 
 
 const WebAPI = defineCollection({
-  loader: glob({ pattern:["**/!(*toc).yml",], base:"./src/content/docs/en/api/reference/webapi"}),
+  loader: glob({
+    // pattern:["**/!(*toc).yml"], 
+    pattern:[""], //Temporary disabled
+    base: "external-content/superoffice-docs/docs/en/api/reference/webapi"
+  }),
 });
 
-const tocFilesInternal = defineCollection({
-  loader: glob({
-    pattern: ["**/toc.yml"],
-    base: "./src/content",
-  }),
-  schema: TocYamlSchema,
-});
+// const tocFilesInternal = defineCollection({
+//   loader: glob({
+//     pattern: ["**/toc.yml"],
+//     base: "./src/content",
+//   }),
+//   schema: TocYamlSchema,
+// });
+
 
 // Collections in external-content (cloned from another GitHub repo)
-
 const tocFilesExternal = defineCollection({
   loader: glob({
-    pattern: ["contribution/**/toc.yml"], // Add in ext superoffice-docs later
+    // pattern: ["**/toc.yml"],
+    pattern: [""], //Temporary disabled
     base: "./external-content",
   }),
   schema: TocYamlSchema,
@@ -127,6 +141,6 @@ export const collections = {
   webapi: WebAPI,
   contribute: contributionRepo,
   external: externalLandingPages,
-  tocInternal: tocFilesInternal,
+  // tocInternal: tocFilesInternal,
   tocExternal: tocFilesExternal,
 };
