23dae482512dae3c6ef13612f8b580e44b65a197
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-07-09 22:39:54 +0530

    feat: updated content schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,69 +1,22 @@
-// This is a special file that Astro will automatically load and use to configure your content collections.
-import { z, defineCollection } from "astro:content";
+// Astro will automatically load the exported collections constant defined in this file and use it to configure content collections.
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { DocsSchema, SimplifiedYamlSchema, TocYamlSchema } from "~/content.schema"
 
+// apiOnly variable is used in the split build to isolate docs/en/api folder content
 const apiOnly = process.env.API_ONLY === 'true';
 
-const DocsSchema = z.object({
 
-  //Mandatory Properties
-  author: z.string().nullable(),
-  date: z.coerce.string().nullable(),
-  uid: z.string().nullable(),
-
-  //Optional Properties
-  title: z.string().optional().nullable(),
-  description: z.string().optional().nullable(),
-  audience: z.string().optional().nullable(),
-  audience_tooltip: z.string().optional().nullable(),
-  category: z.string().optional().nullable(),
-  updated: z.coerce.string().optional().nullable(),
-  version: z.coerce.string().optional().nullable(),
-  version_sofo: z.coerce.string().optional().nullable(),
-  version_devportal: z.coerce.string().optional().nullable(),
-  version_mobile: z.coerce.string().optional().nullable(),
-  translation_type: z.union([z.number(), z.string()]).optional().nullable(),
-  deployment: z.union([z.string(), z.array(z.string())]).optional().nullable(),
-  generated: z.coerce.string().optional().nullable(),
-  keywords: z.union([z.string(), z.array(z.string())]).optional().nullable(),
-  language: z.string().max(2).optional().nullable(),
-  pilot: z.string().optional().nullable(),
-  redirect_url: z.string().optional().nullable(),
-}).passthrough().partial()
-// .partial() is used to make every property optional due to current frontmatter mismatch in some markdown files. Needs to be removed once frontmatter fixed
-
-const SimplifiedYamlSchema = z.object({
-  yamlMime: z.enum(["Category", "SubCategory"]), 
-  title: z.string(),
-  metadata: z.any(),
-}).passthrough(); // Allow all other fields like landingContent, conceptualContent, tools, etc.
-
-const TocItemSchema: z.ZodType<any> = z.lazy(() =>
-  z.object({
-    name: z.string(),
-    uid: z.string().optional(),
-    href: z.string().optional(),
-    topicHref: z.string().optional(),
-    items: z.array(TocItemSchema).optional(),
-  })
-);
-
-const TocYamlSchema = z.object({
-  items: z.array(TocItemSchema),
-});
-
-// Collections in src/content
 const releaseNotes = defineCollection({
-  loader: glob({ pattern: apiOnly ? [""] :  ["*.md", "**/!(*includes*)/*.md"], base: "external-content/superoffice-docs/release-notes" }),
+  loader: glob({ pattern: apiOnly ? [""] : ["*.md", "**/!(*includes*)/*.md"], base: "external-content/superoffice-docs/release-notes" }),
   schema: DocsSchema,
 });
 
 const enDocs = defineCollection({
   loader: glob({
     pattern: apiOnly ? [""] : [
-      "**/*.md",
+      "**/!(**includes**)/*.md",
       "!api/**/*.md",
-      "!**/includes/**/*.md"
     ],
     base: "external-content/superoffice-docs/docs/en",
   }),
@@ -75,16 +28,14 @@ const deDocs = defineCollection({
   schema: DocsSchema,
 });
 
-
 const WebAPI = defineCollection({
   loader: glob({ pattern: apiOnly ? ["**/!(*toc).yml"] : [""], base: "external-content/superoffice-docs/docs/en/api/reference/webapi" }),
 });
 
-// Collections in external-content (cloned from another GitHub repo)
-
-const tocFilesExternal = defineCollection({
+const tocFiles = defineCollection({
   loader: glob({
-    pattern: [
+    pattern: apiOnly ? ["superoffice-docs/docs/en/api/**/toc.yml"] : [
+      "superoffice-docs/docs/en/!(api)/**/toc.yml",
       "superoffice-docs/docs/**/toc.yml",
       "superoffice-docs/release-notes/**/toc.yml",
       "contribution/**/toc.yml",
@@ -106,7 +57,7 @@ const externalLandingPages = defineCollection({
   schema: SimplifiedYamlSchema,
 });
 
-const contributionRepo = defineCollection({
+const contribution = defineCollection({
   loader: glob({
     pattern: apiOnly ? [""] : [
       "**/*.md",
@@ -117,14 +68,13 @@ const contributionRepo = defineCollection({
   schema: DocsSchema,
 });
 
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+//  Export a single `collections` object to register collections
 export const collections = {
   "release-notes": releaseNotes,
   en: enDocs,
   de: deDocs,
   webapi: WebAPI,
-  contribute: contributionRepo,
+  contribute: contribution,
   external: externalLandingPages,
-  toc: tocFilesExternal,
+  toc: tocFiles,
 };
