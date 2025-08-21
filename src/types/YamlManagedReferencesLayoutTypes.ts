2c1cf06540da25ac8217a4e83530d8d12280901d
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-08-21 14:42:19 +0530

    feat: refactor Yaml Managed references

---
+++ src/types/YamlManagedReferencesLayoutTypes.ts
@@ -0,0 +1,24 @@
+export interface indexDictionaryProps {
+    [key: string]: number[];
+}
+
+export interface headingsListProps {
+    [key: string]: headingsItem[];
+}
+
+interface headingsItem {
+    text: string;
+    slug: string;
+}
+
+export interface namespaceClassifiedDataProps {
+    [key: string]: namespaceClassifiedDataItem[];
+}
+
+interface namespaceClassifiedDataItem {
+    uid: string;
+    id: string;
+    summary: string;
+}
+
+
---
+++ src/types/YamlManagedReferencesTypes.ts
@@ -0,0 +1,84 @@
+export interface ManagedReferenceType {
+  items: Item[];
+  references: Reference[];
+}
+
+export interface Item {
+  uid: string;
+  commentId: string;
+  id: string;
+  alias?:string[];
+  parent?: string;
+  children?: string[];
+  langs: string[];
+  name: string;
+  nameWithType: string;
+  fullName: string;
+  type: string;
+  inheritedMembers?:string[];
+  source?: {
+    repo?:String;
+    branch?: string;
+    revision? : String;
+    path?: string;
+    startLine?: string;
+    endLine?:string;
+    isExternal?: boolean;
+  };
+  url?:string;
+  assemblies: string[];
+  namespace?: string;
+  summary?: string;
+  remarks?: string;
+  syntax?: Syntax;
+  example?: string[];
+  inheritance?: string[];
+  derivedClasses?: string[];
+  implements?: string[];
+  overload?:string;
+  "modifiers.csharp"?: string[];
+  "modifiers.vb"?: string[];
+}
+
+export interface Syntax {
+  content: string;
+  "content.vb"?: string;
+  parameters?: Parameter[];
+  return?: {
+    type: string;
+    description: string;
+  };
+}
+
+export interface Parameter {
+  id: string;
+  type: string;
+  description: string;
+}
+
+
+export interface Reference {
+  uid: string;
+  commentId: string;
+  parent?: string;
+  isExternal: boolean;
+  name: string;
+  href?: string;
+  nameWithType: string;
+  fullName: string;
+  definition?: string;
+  "nameWithType.Vb"?: string;
+  "fullName.Vb"?: string;
+  "name.Vb"?: string;
+  "spec.csharp"?: SpecDetail[];
+  "spec.vb"?: SpecDetail[];
+}
+
+
+export interface SpecDetail {
+  uid?: string;
+  name: string;
+  nameWithType: string;
+  fullName: string;
+  isExternal?: boolean;
+}
