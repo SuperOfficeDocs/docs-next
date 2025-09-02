25784acde3a998c7b7710a357198597b7444b70c
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-09-02 11:05:05 +0530

    feat: setup controllers and test health api endpoint

---
+++ Server/Controllers/Health.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult Health() {
+            return Ok(
+                        new { 
+                            status = "ok", 
+                            now = DateTime.UtcNow
+                        }
+                    );
+        }
+    }
+}
