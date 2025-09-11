e1c2c0c37e0659166b98021bc0b48076fbed1f94
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-09-11 19:40:58 +0530

    fix: added conditional check for href

--- ClientApp/src/utils/transformXref.ts
+++ ClientApp/src/utils/transformXref.ts
@@ -2,8 +2,8 @@ export function transformXref(html: string) {
   return html?.replace(
     /<xref href="([^"]+)"[^>]*><\/xref>/g,
     (_match, href) => {
-      const segments = href.split(".");
-      const linkText = segments[segments.length - 1];
+      const segments = href?.split(".");
+      const linkText = segments[segments?.length - 1];
       return `<a class="text-orange-700 not-prose no-underline hover:underline" href="${href}">${linkText}</a>`;
     }
   );
