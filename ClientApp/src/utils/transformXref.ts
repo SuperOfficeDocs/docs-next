10d23daf3e6e9eff5264d5758b0a211f86c3a3a0
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-09-11 19:10:04 +0530

    fix: minor styling and functional fixes in YAML managed references

--- ClientApp/src/utils/transformXref.ts
+++ ClientApp/src/utils/transformXref.ts
@@ -1,11 +1,10 @@
 export function transformXref(html: string) {
-  return html.replace(
+  return html?.replace(
     /<xref href="([^"]+)"[^>]*><\/xref>/g,
     (_match, href) => {
       const segments = href.split(".");
       const linkText = segments[segments.length - 1];
-      const url = `/${href.replace(/\./g, "/")}`;
-      return `<a class="text-orange-700 no-underline hover:underline font-normal" href="${url}">${linkText}</a>`;
+      return `<a class="text-orange-700 not-prose no-underline hover:underline" href="${href}">${linkText}</a>`;
     }
   );
 }
